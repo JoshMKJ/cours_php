8a00b30dd0491189713025694b847afc8145aa2d
matandikojosue6@gmail.com <matandikojosue6@gmail.com>
2025-09-16 15:02:55 +0200

    cours php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2m;
+            text-align: center;
+        
+        }
+    </style>
+</head>
+<body>
+    <header>
+        
+        <h1> <?php echo "coucou, " . $_GET['name'];
+        
+        ?>
+        </h1>
+    </header>
+</body>
+</html>
